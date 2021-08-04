08483f186ec2f1202ef93c1b566f642de2277b82
Pedro Fuentes <pedrofuentes7799@gmail.com>
2021-08-04 13:46:49 -0300

    Subir el archivo app.js a manera de prueba

---
+++ app.js
@@ -0,0 +1,8 @@
+let names = ["ryan", "joe", "john"]
+
+function forloop(names) {
+    for(let i = 0; i < names.length; i++) {
+        console.log("Buenos días " + names[i])
+    }
+}
+forloop(names)
